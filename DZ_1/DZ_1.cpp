97129e8affb59475e930140813bfe43e43f73644
LonelyClover <dan-40in@yandex.ru>
2022-02-14 19:02:30 +0300

    Task DZ_1 solved and pending review

---
+++ DZ_1/DZ_1.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+class Quotient
+{
+  private:
+    int num;
+    unsigned int den;
+
+    int gcd(long long int a, long long int b) const;
+
+  public:
+    Quotient(int n = 0, int d = 1)
+    {
+      if (d == 0)
+      {
+        d = 1;
+      }
+      else if (d < 0)
+      {
+        n *= -1;
+        d *= -1;
+      }
+      
+      const int div = gcd(n, d);
+      
+      num = n / div;
+      den = d / div;
+    }
+
+    void print() const;
+};
+
+/*
+int main()
+{
+  while (true)
+  {
+    int n, d;
+    std::cin >> n >> d;
+    Quotient q(n, d);
+    q.print();
+  }
+  return 0;
+}
+*/
+
+int Quotient::gcd(long long int a, long long int b) const
+{
+  long long int temp;
+  
+  if (a < 0)
+  {
+    a *= -1;
+  }
+
+  if (a < b)
+  {
+    temp = a;
+    a = b;
+    b = temp;
+  }
+
+  while (b != 0) 
+  {
+    a = a % b;
+    if (a < b)
+    {
+      temp = a;
+      a = b;
+      b = temp;
+    }
+  }
+
+  return a;
+}
+
+void Quotient::print() const
+{
+  if (den == 1)
+  {
+    std::cout << num << std::endl;
+  }
+  else
+  {
+    std::cout << num << "/" << den << std::endl;
+  }
+}
