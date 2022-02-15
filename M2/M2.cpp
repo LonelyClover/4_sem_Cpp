a50f9087b7024470059560db250b97f754afe0a6
LonelyClover <dan-40in@yandex.ru>
2022-02-15 13:30:17 +0300

    Task M2 solved and pending review

---
+++ M2/M2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstring>
+
+char* left(char *str, int N)
+{
+
+  char *ptr = str;
+
+  while (*ptr != '\0' && N != 0)
+  {
+    ptr += 1;
+    N -= 1;
+  }
+
+  *ptr = '\0';
+
+  if (N < 0)
+  {
+    return ptr;
+  }
+  else
+  {
+    return str;
+  }
+}
+
+int count_digits(unsigned long num)
+{
+  int count = 0;
+  
+  do
+  {
+    num /= 10;
+    count += 1;
+  }
+  while (num != 0);
+
+  return count;
+}
+
+unsigned long left(unsigned long num, int N)
+{
+  N = count_digits(num) - N;
+  
+  if (N < 0)
+  {
+    return num;
+  }
+
+  while (num > 0 && N > 0)
+  {
+    num /= 10;
+    N -= 1;
+  }
+
+  return num;
+}
+
+///*
+using namespace std;
+
+int main()
+{
+  char token[7] = {};
+  cin.getline(token, 7);
+
+  if (strcmp(token, "string") == 0)
+  {
+    char str[101] = {};
+    cin.getline(str, 101);
+    
+    int N;
+    cin >> N;
+
+    cout << left(str, N) << endl;
+  }
+  else
+  {
+    unsigned long num;
+    int N;
+    cin >> num >> N;
+
+    cout << left(num, N) << endl;
+  }
+
+  return 0;
+}
+//*/
+
