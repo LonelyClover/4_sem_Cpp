cfe3a02d89b91a8ad5a83d095b2eadb9a8a431b9
LonelyClover <dan-40in@yandex.ru>
2022-02-15 13:02:48 +0300

    Task M1 solved and pending review

---
+++ M1/M1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cmath>
+
+int count_symbols(int arg)
+{
+  if (arg < 0)
+  {
+    arg *= -1;
+  }
+  
+  int count = 0;
+  do
+  {
+    arg /= 10;
+    count += 1;
+  }
+  while (arg > 0);
+
+  return count;
+}
+
+int count_symbols(int *arr, int n)
+{
+  int count = 0;
+  
+  for (int i = 0; i < n; i++)
+  {
+    count += count_symbols(arr[i]);
+  }
+
+  return count;
+}
+
+int count_symbols(int *ptr)
+{
+  return count_symbols(*ptr);
+}
+
+int count_symbols(const char *str)
+{
+  int count = 0;
+  
+  while (*str != '\0')
+  {
+    str += 1;
+    count += 1;
+  }
+
+  return count;
+}
+
+int count_symbols(double f)
+{
+  return count_symbols((int) trunc(f));
+}
+
+int count_symbols(double f, int n)
+{
+  return count_symbols(f) + n;
+}
+
+/*
+using namespace std;
+
+int main()
+{
+  int array[7] = {18,20,3,493, 7};
+  cout << count_symbols("mather") << endl;
+  cout << count_symbols(12389) << endl;
+  cout << count_symbols(3.14, 3) << endl;
+  cout << count_symbols(2.71828) << endl;
+  cout << count_symbols(array, 5) << endl;
+  cout << count_symbols(array) << endl;
+  return 0;
+}
+*/
